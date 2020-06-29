007aace066e643feb112df99101e74367e8ca84e
jeananel <jpjeananel@gmail.com>
2020-06-29 14:45:36 -0500

    Agregar ruta al handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,12 +6,18 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/jeananel/social.git/middleware"
+	"github.com/jeananel/social.git/routers"
 	"github.com/rs/cors"
 )
 
 //Managements for set server and route listener ports
 func Managements() {
 	router := mux.NewRouter()
+
+	//Routes
+	router.HandleFunc("/Register", middleware.CheckConnectionToDatabase(routers.Register)).Methods("POST")
+
 	PORT := os.Getenv("PORT")
 
 	if PORT == "" {
--- models/User.go
+++ models/User.go
@@ -14,9 +14,9 @@ type User struct {
 	DateBirth time.Time          `json:"DateBirth" bson:"DateBirth"`
 	Email     string             `json:"Email" bson:"Email"`
 	Password  string             `json:"Password" bson:"Password"`
-	Avatar    string             `json:"Avatar" bson:"Avatar"`
-	Banner    string             `json:"Banner" bson:"Banner"`
-	Biography string             `json:"Biography" bson:"Biography"`
-	Location  string             `json:"Location" bson:"Location"`
-	WebSite   string             `json:"WebSite" bson:"WebSite"`
+	Avatar    string             `json:"Avatar" bson:"Avatar,omitempty"`
+	Banner    string             `json:"Banner" bson:"Banner,omitempty"`
+	Biography string             `json:"Biography" bson:"Biography,omitempty"`
+	Location  string             `json:"Location" bson:"Location,omitempty"`
+	WebSite   string             `json:"WebSite" bson:"WebSite,omitempty"`
 }
